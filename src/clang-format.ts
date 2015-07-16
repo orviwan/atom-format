30bb90f72a495e2db5d0cd041147ee0f840f7e0a
Lambda Chen <lambchen@yeah.net>
2015-07-17 00:31:43 +0800

    Fix bug: default executable path incorrect.

--- src/clang-format.ts
+++ src/clang-format.ts
@@ -4,20 +4,28 @@ import {CompositeDisposable} from 'atom';
 
 export class ClangFormat {
   private subscriptions = new CompositeDisposable();
-
+  private enableAutoFormat = true;
   constructor() {
     this.subscriptions.add(atom.commands.add('atom-workspace', 'atom-format:format', () => {
       var editor = atom.workspace.getActiveTextEditor();
       if (editor) {
         this._format(editor);
+        atom.notifications.addSuccess('Format successfully.');
       }
     }));
+    this.subscriptions.add(
+        atom.commands.add('atom-workspace', 'atom-format:autoFormatOnSave', () => {
+          this.enableAutoFormat = !this.enableAutoFormat;
+          atom.notifications.addInfo(`${this.enableAutoFormat?'Enable':'Disable'} auto format.`);
+        }));
     this.subscriptions.add(atom.workspace.observeTextEditors((editor) => {
       var onSave = editor.getBuffer().onWillSave(() => {
         var autoSave = atom.config.get('atom-format.autoSave');
         var extension = (editor.getPath().match(/\..+?$/) || [])[0];
-        if (autoSave.indexOf(extension) >= 0)
+        if (this.enableAutoFormat && autoSave.indexOf(extension) >= 0) {
           this._format(editor);
+          atom.notifications.addSuccess('Auto format on save successfully.');
+        }
       });
       var onDestroy = editor.getBuffer().onDidDestroy(() => {
         onSave.dispose();
